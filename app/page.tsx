b0a4e152644c5e17a1bca92bb02a3f0507f2ee42
ashish <ashishmangla1991@gmail.com>
2023-07-17 13:10:26 +0530

    remove code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import CardCase from '@/components/cardcase/CardCase'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image"
+import footerStyle from './footer.module.scss'
 
 const Footer = () =>{
 return(
-    <footer>
+    <footer className={footerStyle.footer}>
 
       
 
@@ -11,6 +12,12 @@ return(
 <Image src={`${process.env.IMG_HOST}w_43/v1689574969/venture7/Singapore.svg`} alt="" width={43} height={43}/>
 <Image src={`${process.env.IMG_HOST}w_43/v1689574968/venture7/Australia.svg`} alt="" width={43} height={43}/>
 
+
+
+
+<Image src={`${process.env.IMG_HOST}/v1689579003/venture7/ISO27001-2013.svg`} alt="" width={64} height={64}/>
+<Image src={`${process.env.IMG_HOST}v1689579003/venture7/ISO9001-2015.svg`} alt="" width={64} height={64}/>
+<Image src={`${process.env.IMG_HOST}v1689579003/venture7/ISO20000-1-2018.svg`} alt="" width={64} height={64}/>
     </footer>
 )
 }
