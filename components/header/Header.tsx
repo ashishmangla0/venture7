9ca7258d82c09bbaa28a6a17d7f1a32137037397
ashish <ashishmangla1991@gmail.com>
2023-07-26 21:37:31 +0530

    add new code

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -5,17 +5,17 @@ import { ReactComponent as DropDown } from '@/resources/images/dropdown.svg';
 
 const headerLinks = [
     {
-title: 'services',
-submenu:[
-    {title: 'Product Engineering'}
-]
+        title: 'services',
+        submenu: [
+            { title: 'Product Engineering' }
+        ]
     },
     {
         title: 'solutions'
-            },
-            {
-                title: 'insights'
-                    },
+    },
+    {
+        title: 'insights'
+    },
 
 ]
 
@@ -24,28 +24,28 @@ const Header = () => {
     return (
         <nav className={`${headerStyle.root}`}>
             <div className={`container ${headerStyle.root_container}`}>
-            <Link href={'/'} className={headerStyle.logo}>
-                <Logo  height={47} width={150} />
-            </Link>
+                <Link href={'/'} className={headerStyle.logo}>
+                    <Logo height={47} width={150} />
+                </Link>
 
-            <ul className={`${headerStyle.root_navlist}`}>
-               { headerLinks.map((headerlink) => <li className={`${headerStyle.root_navitem}`}>
-                <Link href={headerlink.title}>{headerlink.title}</Link>
-                {/* {headerlink?.submenu ? <DropDown/>:  ''} */}
-{headerlink?.submenu && <div className=''>
-    I am mega menu
-</div> }
-                
-                
-                </li>
-                
-                )}
-            </ul>
+                {/* <ul className={`${headerStyle.root_navlist}`}>
+                    {headerLinks.map((headerlink) => <li className={`${headerStyle.root_navitem}`}>
+                        <Link href={headerlink.title}>{headerlink.title}</Link>
+                        
+                        {headerlink?.submenu && <div className=''>
+                            I am mega menu
+                        </div>}
+
+
+                    </li>
+
+                    )}
+                </ul> */}
             </div>
-           
 
 
-          
+
+
         </nav>
     )
 }
