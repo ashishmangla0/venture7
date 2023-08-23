618d50c5dfef665f3c513a7f2dcb325df4727a4e
ashish <ashishmangla1991@gmail.com>
2023-08-23 08:59:18 +0530

    add new code

--- components/subscribeform/SubscribeForm.tsx
+++ components/subscribeform/SubscribeForm.tsx
@@ -22,52 +22,48 @@ export default function SubscribeForm() {
       <div className="container">
         <div className={subscribeFormStyle.subscribeform_wrap}>
           <h2>
-          We live your business
+            We live your business
           </h2>
           <p>
-          We understand your business and then become part of it for the success. Get in touch and be the leader of a success story.
+            We understand your business and then become part of it for the success. Get in touch and be the leader of a success story.
           </p>
           <form onSubmit={handleSubmit(onSubmit)} className="grid grid-12">
-
+            <div className="col-span-5">
+              <Input
+                placeholder="Your Email"
+                id="yourEmail"
+                type={"text"}
+                label="Your Email"
+                ariaInvalid={errors?.yourEmail ? true : false}
+                register={register('yourEmail', {
+                  required: "Username is required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
+              />
+              {errors.yourEmail && errors.yourEmail.message}
+            </div>
+            <div className="col-span-5">
             <Input
-              inputWrapperClasses="col-span-5"
-              id="yourEmail"
-              type={"text"}
-              label="Your Email"
-              ariaInvalid={errors?.yourEmail ? true : false}
-              register={register('yourEmail', {
-                required: "Username is required",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: "Invalid email address",
-                },
-              })}
-
-            />
-            {errors.yourEmail && <p>{errors.yourEmail.message}</p>}
-
-            <Input
-              inputWrapperClasses="col-span-5"
+              placeholder="Last Name"
               id="lastname"
               type={"text"}
-              label="lastName"
+              label="Last Name"
               register={register('lastName', { required: "Username is required" })}
             />
             {errors.lastName && "Last name is required"}
-            <div className="col-span-2">
-              <label> a</label>
-              <Button type="submit" buttonClass="" >
-              
-              Submit
-            </Button>
             </div>
             
-
+            <div className="col-span-2">
+              <label>&nbsp;</label>
+              <Button type="submit" buttonClass="button button-secondary" >
+                Submit
+              </Button>
+            </div>
             {/* <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} /> */}
             <Checkbox label="subscribeValue" register={register('aaaa')} />
-
-
-            
           </form>
         </div>
 
